ebc76e83d550085b657fb847cd014116ebb59a5d
MX-05 <62790059+MX-05@users.noreply.github.com>
2021-07-20 15:38:56 +0200

    add utils/image.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,19 @@
 import pygame
 
-from src.utils.button import button
+from src.utils.image import *
+"""
+classes: button(), image(), display().
+button():
+    - image = pygame.image
+    - coords = [int x, int y]
+    - tolleranza = int
+    - hit_box = {
+        "destra": int, 
+        "sinistra": int, 
+        "su": int,
+        "giu": int
+    }
+"""
 
 pygame.init()
 display = pygame.display.set_mode([720, 480])
