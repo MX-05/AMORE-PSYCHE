bcc0cb69eb74e9afefad75c5f1df1e6d3f1364c8
MX-05 <62790059+MX-05@users.noreply.github.com>
2021-07-21 15:56:48 +0200

    add init on sprite classes add background in dislpay classes

--- src/utils/image.py
+++ src/utils/image.py
@@ -31,6 +31,19 @@ class button():
                 return True
 
 class sprite():
+    def __init__(self, coords, path = ""):
+        self.x = coords[0]
+        self.y = coords[1]
+        
+        if path != "":
+            self.image = pygame.image.load(path)
+        else:
+            self.image = pygame.Rect(self.x, self.y, 50, 50)
+            self.width = 50
+            self.height = 50
+        
+        return
+    
     def __str__(self):
         return "a subclasses of pygame.sprite"
     
@@ -44,13 +57,17 @@ class sprite():
         return {"destra": dx, "sinistra": sx, "su": su, "giu": giu}
 
 class display():
-    def __init__(self, size, FPS= -1):
+    def __init__(self, background, size, FPS= -1):
         if FPS <0: 
             self.FPS = 50
         else:
             self.FPS = FPS
     
         self.screen= pygame.display.set_mode(size)
+        if type(background) == type(""):
+            self.background = sprite([0, 0], path=background)
+        if type(background) == type(('r', 'g', 'b')):
+            self.background = sprite([0, 0], path = background)
         return
         
     def update(self):
