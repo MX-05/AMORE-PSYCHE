707267b19fef5a76ab3e2172082cf16d9f63d31f
MX-05 <62790059+MX-05@users.noreply.github.com>
2021-07-26 15:22:11 +0200

    make open method in credits

--- src/credits.py
+++ src/credits.py
@@ -1,4 +1,5 @@
 import utils
+import __init__ as const
 
 text = """
     DEVELOPER: Marco Mazzeo
@@ -11,4 +12,13 @@ text = """
 class credits():
     def __init__(self):
         self.text = text
+        self.opened = False
         
+    def open(self, screen, x, y):
+        backstage = const.size
+        
+        window = utils.button((x, backstage[1]), text)
+        self.window = window
+        for i in range(backstage[1], y):
+            window.y = i
+            window.draw(screen)
