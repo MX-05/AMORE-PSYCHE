5681f73f1e5653d36dfde4ab85e3db78dd3e03ed
MX-05 <62790059+MX-05@users.noreply.github.com>
2021-07-29 18:05:14 +0200

    sistemata la disposizione grafica

--- AMORE_PSICHE.py
+++ AMORE_PSICHE.py
@@ -24,11 +24,17 @@ font = pg.font.SysFont("Arial", 20)
 # ------------------------------------- MENU ------------------------------------- #
 
 # BUTTON SETUP
-play = Button((0,0)).B_text("Play", ["Arial", 50], bg= (255, 0, 0))
-audio = Button((295, 280)).B_text("Audio ON", ["Arial", 25], bg = "Blue", color="White")
+play = Button((0,0)).B_text(
+    " Play ", ["Arial", 50], 
+    bg= (255, 0, 0), radius=20
+)
+audio = Button((295, 280)).B_text(
+    " Audio ON ", ["Arial", 25],
+    bg = (255, 174, 0), color="white", radius = 15 
+)
 
-play.rect.center = [surface.get_rect().centerx/2, surface.get_rect().centery]
-audio.rect.centerx = surface.get_rect().centerx/2
+play.rect.center = [surface.get_rect().centerx, surface.get_rect().centery]
+audio.rect.centerx = surface.get_rect().centerx
 
 menu = pg.sprite.Group()
 
@@ -42,8 +48,11 @@ text_credits = """
 
 """
 
-credits = sprite_font(surface, text_credits, (140, 310), 10, bg=(100, 100, 100))
+credits = sprite_font(surface, text_credits, (10, 0), 15, bg="Black", txt_color="White   ")
 
+credits.rect.bottom = surface.get_rect().bottom - 10
+
+menu.add(credits)
 menu.add(audio)
 menu.add(play)
 move = False
@@ -67,12 +76,13 @@ while True:
             print("FUNGEEEEEEE")
         
         if audio.click(event):
-            if audio.content.lower() == "audio on":
+            if audio.content.lower().strip() == "audio on":
                 surface.fill(display["color"])
-                audio.change_text("Audio off ", bg="white")
+                audio.change_text(" Audio OFF ", bg=(255, 174, 0), txt_color="white", radius=15)
                 audio.rect.centerx = surface.get_rect().centerx
             else:
-                audio.change_text("Audio ON", bg="blue", txt_color= pg.Color("white"))
+                surface.fill(display["color"])
+                audio.change_text(" Audio ON ", bg=(255, 174, 0), txt_color= pg.Color("white"), radius=15)
                 audio.rect.centerx = surface.get_rect().centerx
     
     menu.draw(surface)
