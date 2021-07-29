c12ef06d050229fd241852e92fb4c953d06e011d
MX-05 <62790059+MX-05@users.noreply.github.com>
2021-07-29 08:39:32 +0200

    change background color to white

--- AMORE_PSICHE.py
+++ AMORE_PSICHE.py
@@ -77,10 +77,11 @@ display = {
     'width': 720,
     'height': 480,
     'caption': "AMORE ~ PSICHE",
-    "color": "black"
+    "color": "white"
 }
 pg.display.set_caption(display['caption'])
 surface = pg.display.set_mode((display['width'], display['height']))
+surface.fill(display["color"])
 
 font = pg.font.SysFont("Arial", 20)
 
@@ -88,7 +89,7 @@ font = pg.font.SysFont("Arial", 20)
 
 # BUTTON SETUP
 play = Button("Play", (300, 210), 50, bg=(225, 0, 0))
-audio = Button("Audio ON",(295, 280), 25, bg = "blue")
+audio = Button("Audio ON",(295, 280), 25, bg = "blue", txt_color=pg.Color("white"))
 
 menu = pg.sprite.Group()
 menu.add(audio)
@@ -102,7 +103,7 @@ DOPPIAGGIO: Tiago Pisanti Vieira
 MAPPA ESPOLORAZIONE: Milo Binetti
 """
 
-credits = sprite_font(surface, text_credits, (20, 330), 20)
+credits = sprite_font(surface, text_credits, (20, 330), 20, bg=(100, 100, 100))
 
 menu.add(credits)
 
@@ -125,9 +126,9 @@ while True:
         if audio.click(event):
             if audio.content.lower() == "audio on":
                 surface.fill(display["color"])
-                audio.change_text("Audio off ")
+                audio.change_text("Audio off ", bg="white")
             else:
-                audio.change_text("Audio ON", bg="blue")
+                audio.change_text("Audio ON", bg="blue", txt_color= pg.Color("white"))
     
     # pg.draw.rect(surface, (40, 44, 52), (0, 330, surface.get_width(), surface.get_height()-330))
     
