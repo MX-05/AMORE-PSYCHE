d5bc64d25933fc451a41f32ca75a37240dfd19e8
MX-05 <62790059+MX-05@users.noreply.github.com>
2021-07-31 01:01:13 +0200

    mah..

--- AMORE_PSICHE.py
+++ AMORE_PSICHE.py
@@ -67,6 +67,8 @@ credits = sprite_font(
 )
 
 credits.rect.bottom = surface.get_rect().bottom - 10
+credits.image.set_alpha(157)
+credits.image = credits.image.convert_alpha()
 
 menu.add(credits)
 
@@ -74,11 +76,14 @@ menu.add(credits)
 # SKIN SETUP
 skin = {
     "button": Button((0, 0)).B_text(
-        " SKIN ", [font, 35], 
+        " SKIN ", [font, 25], 
         bg = (255, 174, 0), color="white", radius=15
     ),
     "image": Button((865, 100), path = "./assets/pg_pattuglie/pg_tigre.png"),
-    "menu": Button((0, 0)) # TODO: set pos and size 
+    "menu": Button(
+        (0, 0),                         # POS
+        size = [752, 641]               # SIZE
+    )
 }
 
 skin["image"].image = pg.transform.scale(skin["image"].image, (300, 400))
@@ -117,7 +122,7 @@ class skin_animation(Button):
         # SET ANIMATION VARIABLES    
         Ypoint = self.rect.centery
         distance = Ypoint - self.center -25 
-        pg.time.delay(25)
+        #pg.time.delay(25)
         
         # GO DOWN
         if Ypoint >= self.center -26 and self.move == True:
@@ -134,15 +139,31 @@ class skin_animation(Button):
         
 skin["asset"] = skin_animation(skin["image"].pos, skin["image"].image)
 
-# SKIN MEN  U
-# TODO: draw rectangle
-# TODO: blit skins 
+# SKIN MENU
+
+# draw rect skin menu
+pg.draw.rect(skin["menu"].image, ("white"), skin["menu"].rect, border_radius= 30)
+skin["menu"].image = skin["menu"].image.convert()
+skin["menu"].image.set_colorkey((0, 0, 0))
+
+skin["menu"].rect.center = surface.get_rect().center  # POS
+skin["menu"].rect.top = surface.get_rect().bottom# POS
+
+# animation variable
+def restart_animation():
+    global exit_SkinMenu, open_SkinMenu, click
+    global vel, a
+    exit_SkinMenu = False
+    open_SkinMenu = False
+    click = False
+    vel = 0
+    a = 0.5 
+restart_animation()
 # TODO: add select skins buttons
 # TODO: add exit button 
 
 menu.add(skin["asset"])
 menu.add(skin["button"])
-menu.add(skin["menu"])
 
 while True:
     
@@ -160,17 +181,43 @@ while True:
         if event.type == pg.MOUSEBUTTONDOWN:
             print(mouse.get_pos())
         
-        if play.click(event):
+        if play.click(event) and not click:
             print("FUNGEEEEEEE")
         
-        if audio.click(event):
+        if audio.click(event) and not click:
             if audio.content.lower().strip() == "audio on":
                 audio.change_text(" Audio OFF ", bg=(255, 174, 0), txt_color="white", radius=15)
+                audio.image.set_alpha(157)
+                audio.image = audio.image.convert_alpha()
                 audio.rect.centerx = surface.get_rect().centerx
             else:
                 audio.change_text(" Audio ON ", bg=(255, 174, 0), txt_color= pg.Color("white"), radius=15)
                 audio.rect.centerx = surface.get_rect().centerx
-           
+        
+        if skin["button"].click(event) and not click:
+            open_SkinMenu = True
+            menu.add(skin["menu"])
+        
+        if event.type == pg.MOUSEBUTTONDOWN and not skin["menu"].rect.collidepoint(mx, my) and click:
+            exit_SkinMenu = True
+        
+    if exit_SkinMenu:
+        if skin["menu"].rect.y >= surface.get_rect().bottom:
+            menu.remove(skin["menu"])
+            restart_animation()
+        else:                
+            vel += a
+            skin["menu"].rect.y += vel 
+    
+    if open_SkinMenu:
+        if skin["menu"].rect.center >= surface.get_rect().center:
+            vel += a
+            skin["menu"].rect.y -= vel
+            
+            if pg.MOUSEBUTTONDOWN in pg.event.get(eventtype=pg.MOUSEBUTTONDOWN): print("click")
+        else:
+            restart_animation()
+            click = True
     
     surface.blit(display["background"], (0,0))
     menu.draw(surface)
